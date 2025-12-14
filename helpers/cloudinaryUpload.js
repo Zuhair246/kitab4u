e27b4bfd2500a45b328a6b14f2274f35390d22ce
Ahmed Zuhair <zuhair.job.08@gmail.com>
2025-12-14 14:34:19 +0530

    feat: Cloudinary added

---
+++ helpers/cloudinaryUpload.js
@@ -0,0 +1,16 @@
+import cloudinary from "../config/cloudinary.js";
+
+export const uploadToCloudinary = async (buffer, folder) => {
+    return await new Promise( (resolve, reject) => {
+        cloudinary.uploader.upload_stream(
+            {
+                folder,
+                resource_type: "image",
+            },
+            (error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+            }
+        ).end(buffer);
+    });
+};
